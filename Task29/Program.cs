9a26591cfd060ae45a69918e8ab7a32c88dc4add
EkaterinaAsanova <u-kat@mail.ru>
2022-12-09 21:14:54 +0500

    Генерация массива и вывод его на экран

---
+++ Task29/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Random Array = new Random(); 
+int[] mas = new int [8];
+    for (int i = 0; i < mas.Length; i++)
+    {
+    mas [i] = new Random().Next(-100, 101);
+    }  
+Console.WriteLine ("Сгенерировано 8 случайных чисел от -100 до 100:");
+Console.WriteLine (string.Join(",", mas));
